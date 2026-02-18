5eab4ab0a8b107dc4009ad7542f140855012ed18
ninlawan <ninlawan2212@gmail.com>
2026-02-18 14:37:55 +0700

    loop question and check answer function

--- learn.js
+++ learn.js
@@ -40,8 +40,31 @@ const question = [
         english: "No",
         pronunciation: "mai chai"
     }
-] ;
+] 
 
 console.log("total questions:", question.length);
 console.log("first question:", question[0]);
 console.log("second question:", question[1]);
+
+for (let i = 0; i < question.length; i++) {
+    console.log("thai:  "+question[i].thai);
+    console.log("english:  "+question[i].english);
+    console.log("how to say:  "+question[i].pronunciation);
+    
+}
+
+function chexkAnswer(question, userAnswer) {
+    const correctAnswer = question.english;
+    if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+        score += 10;
+        return("Correct!");
+        
+    } else {
+        console.log("Incorrect. The correct answer is: " + correctAnswer);
+    }
+}
+//test the function
+console.log(chexkAnswer(question[0], "Hello"));
+console.log("current score:", score);
+console.log(chexkAnswer(question[1], "sorry"));
+console.log("current score:", score);
